b7712e35adebfe575ea7ea5fa4c97a4e3d01a9bd
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-01-12 11:17:50 +0100

    struttura base spring-data

---
+++ src/test/java/Domenico/GestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package Domenico.GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
